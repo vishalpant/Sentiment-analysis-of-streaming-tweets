8fae64f0af1901aa8d60fb3757146b2172157630
vishal <vishalpant.pant65@gmail.com>
2019-08-04 17:45:07 +0530

    added confusion matrix

--- main(streaming).py
+++ main(streaming).py
@@ -20,12 +20,12 @@ if __name__ == '__main__':
     try:
         woeid = get_woeid.get_woeid(country)
         trends = twitter_streaming.get_trendy(int(woeid))
-        Hashtags = [x['name'].encode('utf-8') for x in trends[0]['trends']]
+        Hashtags = [str(x['name']) for x in trends[0]['trends']]
         print("Trends in your country:")
         Hash_tags = ", ".join(Hashtags)
         print(Hash_tags)
-    except:
-        print("Error getting current trends of your country")
+    except Exception as e:
+        print("Error getting current trends of your country.\nEncountered an exception while getting the trends in your area.\n\t%s"%e)
     track, number = str(input("Enter track and number of tweets(use \",\" as delimiter):")).split(",")
     tweets, sentiments = twitter_streaming.get_data(track, int(number))
     data = data + tweets
@@ -34,9 +34,13 @@ if __name__ == '__main__':
     classifier = Sentiment.learn(tf_idf[:1600000], target[:1600000])
     prediction = Sentiment.predict(tf_idf[1599999:], classifier)
     prediction = [int(x) for x in prediction]
-    if sum(prediction)/(float)(len(prediction)) > 2:
-        print("\nAccording to the recent %d tweets, people are having a POSITIVE reaction towards %s"%(int(number),track))
-    elif sum(prediction)/(float)(len(prediction)) < 2:
-        print("\nAccording to the recent %d tweets, people are having a NEGATIVE reaction towards %s"%(int(number),track))
-    else:
-        print("\nAccording to the recent %d tweets, people are having a NEUTRAL reaction towards %s"%(int(number),track))
+    print("\nTotal tweets analyzed: %d"%len(prediction))
+    total_positive = 0
+    total_negative = 0
+    for i in prediction:
+        if int(i) == 0:
+            total_negative+=1
+        else:
+            total_positive+=1
+    print("Positive : %.2f%c"%(total_positive/(float)(len(prediction))*100, '%'))
+    print("Negative : %.2f%c"%(total_negative/(float)(len(prediction))*100, '%'))
--- test.py
+++ test.py
@@ -4,16 +4,20 @@ import Sentiment
 import Filter
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
 
 def evaluate_model(target_true,target_predicted):
+    print("The confusion matrix is as follows:")
+    print(confusion_matrix(target_true,target_predicted))
+    print("The classification report is as follows:")
     print(classification_report(target_true,target_predicted))
     print("The accuracy score is {:.2%}".format(accuracy_score(target_true,target_predicted)))
 
 
 if __name__ == "__main__":
     if not os.path.isfile("sentiments.pickle") or not os.path.isfile("tweets.pickle"):
-        datafile = str(input("Enter path of training data(Should be in .csv)"))
+        datafile = str(input("Enter path of training data(Should be in .csv):"))
         # replace 4th parameter with the column number of your tweets
         # replace 5th parameter with the column number of your sentiments
         data, target = load.load_data(datafile, ",", '"', 5, 0)
